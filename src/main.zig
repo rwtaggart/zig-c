c79778ef92136140d87611779363a21d92147eb0
Richard <12618621+rwtaggart@users.noreply.github.com>
2025-06-15 18:42:11 -0400

    feat: update main with all sample libs

--- src/main.zig
+++ src/main.zig
@@ -1,29 +1,31 @@
+//! MAIN – Test C API example
 //!
-//! zig test
 //! QUESTION:
 //! How do I include c-library with cImport and zig build?
 //! https://discord.com/channels/605571803288698900/1274576428452548731
-//!
-//! BUILD
-//! zig build-exe -I /opt/homebrew/Cellar/sqlite/3.46.0/include -L /opt/homebrew/Cellar/sqlite/3.46.0/lib -lsqlite3 ./src/note.zig
 //!
 //! Note: C-Pointer [*c] can be cast into Optional Pointer ?*
 //! Note: Null-terminated C-string literals must be translated into Zig string literals with std.mem.span()
 //!
 //! USAGE:
-//!   zig test -I /opt/homebrew/Cellar/sqlite/3.46.0/include -L /opt/homebrew/Cellar/sqlite/3.46.0/lib -lsqlite3 ./src/sqlite_db.zig
-//!
+//! Run tests:
 //! zig test src/main.zig -lc -I src
+//!
+//! Run main:
 //! zig build-exe src/main.zig -lc -I src
 //! zig build-exe src/main.zig src/c_api.c -lc -I src   # ALT WITH HEADER FILE
 
 const std = @import("std");
 
-// const c_api = @import("c_api");
+const z_api = @import("./root.zig");
 
 const c_api = @cImport({
     // @cInclude("c_api.h");
     @cInclude("c_api.c");
+});
+
+const a_api = @cImport({
+    @cInclude("a_api.c");
 });
 
 pub fn main() !void {
@@ -39,9 +41,20 @@ pub fn main() !void {
 
     try stdout.print("Run `zig build test` to run the tests.\n", .{});
 
-    try bw.flush(); // don't forget to flush!
-
+    const c = c_api.special_fn(12);
     c_api.special_print(12);
+    c_api.special_print(c);
+
+    const z = z_api.special_fn(22);
+    try z_api.special_print(22);
+    try z_api.special_print(z);
+
+    const a = a_api.a_special_fn(11);
+    try stdout.print("a_lib number: {d}\n", .{a});
+    // _ = a;
+    // try stdout.writeAll("a_lib number: \n");
+
+    try bw.flush(); // don't forget to flush!
 }
 
 test "simple test" {
@@ -51,14 +64,26 @@ test "simple test" {
     try std.testing.expectEqual(@as(i32, 42), list.pop());
 }
 
+test "a test" {
+    const res = a_api.a_special_fn(11);
+    try std.testing.expectEqual(@as(u32, 132), res);
+}
+
 test "c test" {
     const res = c_api.special_fn(12);
     try std.testing.expectEqual(@as(u32, 144), res);
 }
 
 test "c print test" {
-    // const res = c_api.special_fn(12);
-    // try std.testing.expectEqual(@as(u32, 144), res);
-
     c_api.special_print(12);
 }
+
+test "z test" {
+    const res = z_api.special_fn(22);
+    try std.testing.expectEqual(@as(u32, 264), res);
+}
+
+// test "z print test" {
+//     // FIXME: stdout breaks "zig build test" runner
+//     try z_api.special_print(22);
+// }
