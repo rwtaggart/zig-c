0d4560e1eb70680b95cccfb599335c10fa88fd8a
Richard <12618621+rwtaggart@users.noreply.github.com>
2025-06-15 18:41:39 -0400

    fix: add zig lib equivalent example

--- src/root.zig
+++ src/root.zig
@@ -1,8 +1,20 @@
+//! API equivalent of c_api.h
+//! Implemented in native Zig
+
 const std = @import("std");
 const testing = std.testing;
 
-export fn add(a: i32, b: i32) i32 {
+pub fn add(a: i32, b: i32) i32 {
     return a + b;
+}
+
+pub fn special_fn(a: u32) u32 {
+    return 12 * a;
+}
+
+pub fn special_print(a: u32) !void {
+    const stdout = std.io.getStdOut().writer();
+    try stdout.print("Dis a number: {d}\n", .{a});
 }
 
 test "basic add functionality" {
